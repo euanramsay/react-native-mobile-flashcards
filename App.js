d07ec1f4120cb5872ccd0f228fa8daaf82a9fbf5
Euan Ramsay <euancramsay@aol.com>
2018-08-26 11:32:36 +0100

    feat: working on navigation.

--- App.js
+++ App.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createBottomTabNavigator } from 'react-navigation'
+
+function Home () {
+  return (
+    <View style={styles.container}>
+      <Text>HOME</Text>
+    </View>
+  )
+}
+
+function CreateQuiz () {
+  return (
+    <View style={styles.container}>
+      <Text>CREATE QUIZ</Text>
+    </View>
+  )
+}
+
+function Settings () {
+  return (
+    <View style={styles.container}>
+      <Text>SETTINGS</Text>
+    </View>
+  )
+}
+
+const Tabs = createBottomTabNavigator({
+  Home: {
+    screen: Home
+  },
+  CreateQuiz: {
+    screen: CreateQuiz
+  },
+  Settings: {
+    screen: Settings
+  }
+})
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+  render () {
+    return <Tabs />
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4286f4',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})

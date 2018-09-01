5ebe54f4ac35afa0c413119276cb9d293eaaa028
Euan Ramsay <euancramsay@aol.com>
2018-09-01 12:44:17 +0100

    feat: Got all basic features working.

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,40 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { styles } from '../utils/styles'
 
-const Quiz = props => {
-  const { title, questions } = props.navigation.state.params
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Quiz: {title}</Text>
-      <Text>
-        {questions.length} {questions.length === 1 ? 'card' : 'cards'}
-      </Text>
-      <TouchableOpacity
-        style={styles.clickable}
-        onPress={() =>
-          props.navigation.navigate('QuizStart', {
-            title,
-            questions
-          })}
-      >
-        <Text>Start quiz</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.clickable}
-        onPress={() => props.navigation.navigate('NewQuestion', { title })}
-      >
-        <Text>Add new question</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.clickable}
-        onPress={() => props.navigation.navigate('Home')}
-      >
-        <Text>Home</Text>
-      </TouchableOpacity>
-    </View>
-  )
+class Quiz extends Component {
+  render () {
+    const { questions, navigation } = this.props
+    const { title } = navigation.state.params
+    console.log(this.props)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Quiz: {title}</Text>
+        <Text>
+          {questions.length} {questions.length === 1 ? 'card' : 'cards'}
+        </Text>
+        {questions.length !== 0
+          ? <TouchableOpacity
+            style={styles.clickable}
+            onPress={() =>
+                navigation.navigate('QuizStart', {
+                  title,
+                  questions
+                })}
+            >
+            <Text>Start quiz</Text>
+          </TouchableOpacity>
+          : null}
+        <TouchableOpacity
+          style={styles.clickable}
+          onPress={() =>
+            navigation.navigate('NewQuestion', { title })}
+        >
+          <Text>Add new question</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.clickable}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Text>Home</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
-export default connect()(Quiz)
+const mapStateToProps = (state, ownProps) => ({
+  questions: state.card[ownProps.navigation.state.params.title].questions
+})
+
+export default connect(mapStateToProps)(Quiz)
--- components/QuizStart.js
+++ components/QuizStart.js
@@ -6,22 +6,88 @@ import { styles } from '../utils/styles'
 class QuizStart extends Component {
   state = {
     score: 0,
-    questionNumber: 0
+    questionNumber: 0,
+    toggleAnswer: false
   }
 
   // TODO: Implement answering of questions and scoring.
 
   render () {
-    const { title, questions } = this.props.navigation.state.params
-    const { questionNumber } = this.state
-    return (
-      <View style={styles.container}>
-        <Text style={styles.heading}>{questions[questionNumber].question}</Text>
-        <TouchableOpacity style={styles.clickable} onPress={() => null}>
-          <Text>Answer</Text>
-        </TouchableOpacity>
-      </View>
-    )
+    const { questions } = this.props.navigation.state.params
+    const { navigation } = this.props
+    const { questionNumber, toggleAnswer, score } = this.state
+    console.log(this.state)
+    console.log(questions.length)
+    console.log(navigation)
+    if (questionNumber < questions.length) {
+      return (
+        <View style={styles.container}>
+          {toggleAnswer
+            ? <View>
+              <Text>Answer</Text>
+              <Text style={styles.heading}>
+                {questions[questionNumber].answer}
+              </Text>
+              <TouchableOpacity
+                style={styles.clickable}
+                onPress={() =>
+                    this.setState(prevState => ({
+                      toggleAnswer: !prevState.toggleAnswer,
+                      score: prevState.score + 1,
+                      questionNumber: prevState.questionNumber + 1
+                    }))}
+                >
+                <Text>Correct?</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.cancel}
+                onPress={() =>
+                    this.setState(prevState => ({
+                      toggleAnswer: !prevState.toggleAnswer,
+                      questionNumber: prevState.questionNumber + 1
+                    }))}
+                >
+                <Text>Unlucky?</Text>
+              </TouchableOpacity>
+            </View>
+            : <View>
+              <Text style={styles.heading}>
+                {questions[questionNumber].question}
+              </Text>
+              <TouchableOpacity
+                style={styles.clickable}
+                onPress={() =>
+                    this.setState(prevState => ({
+                      toggleAnswer: !prevState.toggleAnswer,
+                      toggleQuestion: !prevState.toggleQuestion
+                    }))}
+                >
+                <Text>Answer</Text>
+              </TouchableOpacity>
+            </View>}
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.heading}>
+            You have finished the quiz with a score of {score}
+          </Text>
+          <TouchableOpacity
+            style={styles.clickable}
+            onPress={() => navigation.navigate('Quiz')}
+          >
+            <Text>Try quiz again</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.clickable}
+            onPress={() => navigation.navigate('Home')}
+          >
+            <Text>Home</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
   }
 }
 
